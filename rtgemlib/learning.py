fixed parents count vector computation + more efficient + code cleaning

rtgemlib/learning.py
@@ -2,28 +2,61 @@ import pandas as pd
 import numpy as np
 import copy
 from tqdm import tqdm
+import scipy as sc
 
-from .parentCount import get_nodes_pcv_from_data, get_node_pcv_from_data, lambdas_from_pcv_serie
-
-
-def lambda_duration(time_serie):
-    return (time_serie - time_serie.shift().fillna(0)).sum()
+from .parentCount import get_parents_count_vector
+from .sampling import set_nodes_parents_counts
 
 
-def get_count_duration_df(data, lambda_col='lambda_t', time_col='time', event_col='event'):
-    lambda_vals = data[lambda_col].unique()
-    lambda_count_duration_df = data.drop_duplicates([event_col, 'pcv'])[[event_col, 'pcv',
-                                                                         lambda_col]]
-    data_idx = data.set_index(
-        ['event', lambda_col]).sort_index(level=1)
+def duration(params_df, model, t_max, node=None):
+    params_df.loc[:, 'duration'] = 0
+    params_df.set_index(['event', 'pcv'], inplace=True)
+    nodes = model.dpd_graph.nodes
+    if node is not None:
+        nodes = [node]
 
-    lambda_count_duration_df.loc[:, 'duration'] = lambda_count_duration_df[['event', lambda_col]].apply(
-        lambda lm: lambda_duration(data_idx.loc[(lm['event'], lm[lambda_col]), 'time']), axis=1)
+    for n in nodes:
+        t_ = 0
 
-    lambda_count_duration_df.loc[:, 'count'] = lambda_count_duration_df[['event', lambda_col]].apply(
-        lambda lm: data_idx.loc[(lm['event'], lm[lambda_col])].shape[0], axis=1)
+        while t_ < t_max:
 
-    return lambda_count_duration_df
+            pcv, exp_t = get_parents_count_vector(
+                model.get_parents_count(n), t_, t_max)
+
+            params_df['duration'].loc[(n, pcv)] += min(exp_t, t_max) - t_
+            t_ = exp_t
+
+    # l_drt = 0
+    # t_i_pred = 0
+    # i = 0
+    # while i < len(ts):
+
+    #     t_i = ts[i]
+    #     l_drt += sc.integrate.quad(lambda t_: (get_parent_configuration(node=l, tgems=model, t=t_)[0] == j) * 1.0,
+    #                                t_i_pred, t_i)[0]
+    #     t_i_pred = t_i
+    #     i += 1
+    params_df.reset_index(inplace=True)
+
+
+def get_count_duration_df(data, model, t_max, node=None):
+    nodes = model.dpd_graph.nodes
+    if node is not None:
+        nodes = [node]
+
+    params_df = pd.DataFrame([(node, pcv, lm) for node in nodes
+                              for pcv, lm in model.dpd_graph.nodes[node]['lambdas'].items()], columns=['event', 'pcv', 'lambda_t'])
+
+    lambda_counts = data.groupby(['event', 'pcv'])['time'].size()
+
+    ts = np.append(data['time'].values, t_max)
+    # params_df.loc[:, 'duration'] = params_df[['event', 'pcv']].apply(
+    #     lambda row: duration(ts, model, row['event'], row['pcv']), axis=1)
+    duration(params_df, model, t_max, node)
+    params_df.loc[:, 'count'] = params_df[['event', 'pcv']].apply(
+        lambda row: lambda_counts.get(tuple(row), 0), axis=1)
+
+    return params_df
 
 
 def compute_logLikelihood(count_duration_df):
@@ -33,65 +66,76 @@ def compute_logLikelihood(count_duration_df):
     return log_likelihood
 
 
-def set_pcv_lambda_t(model, data, event_col='event', time_col='time'):
-    data.loc[:, 'pcv'] = get_nodes_pcv_from_data(
-        model, data, event_col, time_col).values
+def set_pcv_lambda_t(model, data, t_max, event_col='event', time_col='time'):
+    data['pcv'] = data[['event', 'time']].apply(lambda row: get_parents_count_vector(
+        model.get_parents_count(row['event']), row['time'], t_max)[0], axis=1)
 
-    data.loc[:, 'lambda_t'] = lambdas_from_pcv_serie(
-        model, data)
+    data.loc[:, 'lambda_t'] = data[['event', 'pcv']].apply(
+        lambda row: model.get_lambda(node=row['event'], pcv=row['pcv']), axis=1)
 
 
-def LogLikelihood(model, observed_data, time_col='time', event_col='event'):
-    set_pcv_lambda_t(model, observed_data)
-    lambda_count_duration_df = get_count_duration_df(observed_data)
+def set_nodes_timeseries(model, data):
+    data.groupby('event')['time'].apply(
+        lambda ts: model.set_node_timeserie(ts.name, ts.values))
+
+    return model
+
+
+def LogLikelihood(model, observed_data, time_col='time', event_col='event', t_max=100):
+    model = set_nodes_timeseries(model, observed_data)
+    model = set_nodes_parents_counts(model, model.dpd_graph.nodes)
+
+    set_pcv_lambda_t(model, observed_data, t_max)
+    lambda_count_duration_df = get_count_duration_df(
+        observed_data, model, t_max)
+
     return compute_logLikelihood(lambda_count_duration_df)
 
 
-def scoreBic(model, observed_data, time_col='time'):
-    likelihood = LogLikelihood(model, observed_data)
+def scoreBic(model, observed_data, time_col='time', t_max=100):
+    likelihood = LogLikelihood(model, observed_data, t_max)
 
-    temps = observed_data[time_col]
-    duree = temps.max() - temps.min()
-
-    bic_score = likelihood - model.size() * np.log(duree)
+    bic_score = likelihood - model.size() * np.log(t_max)
 
     return bic_score
 
 
-def mle_lambdas(model, data, time_col='time', event_col='event'):
+def mle_lambdas(model, data, time_col='time', event_col='event', t_max=10):
 
-    set_pcv_lambda_t(model, data)
-    count_and_duration = get_count_duration_df(data)
+    set_pcv_lambda_t(model, data, t_max)
+    count_and_duration = get_count_duration_df(data, model, t_max)
 
     count_and_duration.loc[:, 'lambda_t'] = count_and_duration[
-        'count'] / count_and_duration['duration']
+        'count'] / (count_and_duration['duration'])
 
     for _, row in count_and_duration.iterrows():
         model.set_lambda(row['event'], row['pcv'], row['lambda_t'])
 
-    return count_and_duration
+    return count_and_duration.fillna(0)
 
 
 def get_node_LogLikelihood(model, cnt_drt_df, node):
     return compute_logLikelihood(cnt_drt_df[cnt_drt_df['event'] == node])
 
 
-def LocaleLogLikelihood(model, data, baseLogL, nodeLogL, changed_node, optimize_lambdas=False):
+def LocaleLogLikelihood(model, data, t_max, baseLogL, nodeLogL, changed_node, optimize_lambdas=False):
     # data.set_index('event', inplace=True)
     # count_duration_df.set_index('event', inplace=True)
     changed_node_data = data[data['event'] == changed_node]
-
-    changed_node_data.loc[:, 'pcv'] = get_node_pcv_from_data(model,
-                                                             data,
-                                                             changed_node_data,
-                                                             event=changed_node).values
+    model = set_nodes_parents_counts(model, changed_node)
+    set_pcv_lambda_t(model, changed_node_data, t_max)
+    data.loc[data['event'] == changed_node, :] = changed_node_data
 
     changed_node_cnt_drt_df = get_count_duration_df(
-        changed_node_data).reset_index(drop=True)
+        changed_node_data, model, t_max, changed_node).reset_index(drop=True)
 
     if optimize_lambdas:
-        changed_node_cnt_drt_df.loc[:, 'lambda_t'] = changed_node_cnt_drt_df[
-            'count'] / changed_node_cnt_drt_df['duration']
+        changed_node_cnt_drt_df.loc[:, 'lambda_t'] = (changed_node_cnt_drt_df[
+            'count'] / changed_node_cnt_drt_df['duration']).fillna(1e-4)
+        # changed_node_cnt_drt_df.loc[
+        #     :, 'lambda_t'] = changed_node_cnt_drt_df.loc[:, 'lambda_t'] / changed_node_cnt_drt_df.loc[:, 'lambda_t'].sum()
+        # # changed_node_cnt_drt_df['lambda_t'] = changed_node_cnt_drt_df[
+        # #     'lambda_t']
 
     return baseLogL - nodeLogL + compute_logLikelihood(changed_node_cnt_drt_df), changed_node_cnt_drt_df
 
@@ -149,9 +193,13 @@ def backward_neighbors_gen(rtgem, data, cnt_drt_df, LogL, size_log_td, log_td):
                 yield rtgem.inverse_split_operator, [edge, i_tm], logL_ngr, size_log_td_ngbr, cnt_drt_df
 
 
-def backwardSearch(model, data):
-    set_pcv_lambda_t(model, data)
-    lambdas_count_duration_df = get_count_duration_df(data)
+def backwardSearch(model, data, t_max=100):
+    model = set_nodes_timeseries(model, observed_data)
+    model = set_nodes_parents_counts(model, model.dpd_graph.nodes)
+
+    set_pcv_lambda_t(model, data, t_max)
+
+    lambdas_count_duration_df = get_count_duration_df(data, model, t_max)
 
     LogL = compute_logLikelihood(lambdas_count_duration_df)
     log_td = np.log(data.iloc[-1]['time'] - data.iloc[0]['time'])

rtgemlib/parentCount.py
@@ -4,164 +4,67 @@ import numpy as np
 from .rtgem import RTGEM
 
 
-def count_timescale(df, timescales_parent):
-    """
-    df: Dataframe, indexé par la serie temporelle du parent.
-    timescales_parent: tableau contenant les timescales du parent. (tableau d'intervalle semi-ouvert)
+def get_parents_count_vector(parents_count, t, t_max):
+    pcv = ()
+    t_expr = t_max
+    for i, parent_count in enumerate(parents_count):
+        cv, t_e = get_parent_count_vector(
+            parent_count, t, t_max)
 
-    retourne un DF indiquant la configuration du parent à chaque t, et le temps de début et d'expiration de la configuration.
-    """
-    # pour chaque instant t où le parent s'est réalisé
-    for i, t in enumerate(df.index):
-        # pour chaque timescale
-        for tm_num, (a, b) in enumerate(timescales_parent):
-            # la colonne indiquant si le parent a eu lieu dans la timscale
-            # timescales_parent[tm_num]
-            tm_col = df.columns[tm_num]
-            # df est indexé par les timestamps auxquels la parent a eu lieu,
-            # les timestamps <= t (t est le iéme index) sont séléctionnée,
-            # les timestampes sont "mappée" à un tableau de booléen qui indique pour chacun si il appartient ou non au timescale,
-            # la somme du tableau correspond au nombre de fois où le parents s'est réalisé dans la timescale.
-            # Cette somme est seuilé par 1.
-            df.loc[t, tm_col] = (
-                ((df.index[:i + 1] > t - b) & (df.index[:i + 1] <= t - a)).sum() >= 1) * 1
-            # si le parent s'est réaliser dans [a, b[,
-            # on doit ajouter le temps d'expiration de la configuration
-            if df.loc[t, tm_col]:
-                # la durée maximale (ne prend pas en compte les futures
-                # réalisatio)
-                duration = (b - a)
-                # le temps d'expiration
-                df.loc[t, 't_expr'] = min(
-                    t + duration, df.loc[t, 't_expr'])
-                df.loc[(df.index.values > t) & (
-                    df.index < t + duration), tm_col] = 1
-    return df
+        pcv += cv
+        t_expr = min(t_expr, t_e)
+
+    return pcv, t_expr
 
 
-def init_parent_count_df(timeserie, timescales,):
-    return pd.DataFrame(data=[], index=timeserie,
-                        columns=['[{},{}['.format(tm[0], tm[1]) for tm in timescales] + ['expr([{},{}[)'.format(tm[0], tm[1]) for tm in timescales] + ['t_expr'])
+def get_parent_count_vector(parent_count, t, t_max):
+    cv = ()
+    t_expr = t_max
+
+    for par_act_expr in parent_count:
+        if par_act_expr.shape[0] > 0:
+
+            res = par_act_expr[(t >= par_act_expr[:, 0]) &
+                               (t < par_act_expr[:, 1])]
+            if res.shape[0] > 0:
+                cv += (1,)
+                t_expr = min(t_expr, res[0, -1])
+            else:
+                cv += (0,)
+                act_t = par_act_expr[t < par_act_expr[:, 0]]
+                if(act_t.shape[0] > 0):
+                    t_expr = min(t_expr, act_t[0, 0])
+        else:
+            cv += (0,)
+
+    return cv, t_expr
 
 
-def updates_parent_count_df(parent_count_df, tms,
-                            t_n_pa):
+def init_parents_count(timeserie, timescales):
+    return [np.empty((0, 2)) for tm in timescales]
 
-    t_pa = np.append(parent_count_df.values, t_n_pa)
+
+def set_parent_count(pa_timeserie, pa_timescales, t_max):
+    parent_count = init_parents_count(pa_timeserie, pa_timescales)
+    for t in pa_timeserie:
+        parent_count = updates_parents_count(
+            parent_count, pa_timescales, t, t_max)
+
+    return parent_count
+
+
+def updates_parents_count(parent_count, tms,
+                          t_n_pa, t_max):
 
     for tm_num, (a, b) in enumerate(tms):
-        tm_col = parent_count_df.columns[tm_num]
-        t_expr_tms = t_n_pa + (b - a)
-        tm_expr_col = 'expr({})'.format(tm_col)
 
-        if parent_count_df.shape[0] == 0:
-            parent_count_df.loc[t_n_pa, tm_col] = (
-                ((t_pa > t_n_pa - b) & (t_pa <= t_n_pa - a)).sum() >= 1) * 1
+        t_act_tms = min(t_n_pa + a, t_max)
+        t_expr_tms = min(t_n_pa + b, t_max)
+
+        if len(parent_count[tm_num]) > 0 and t_act_tms <= parent_count[tm_num][-1, -1]:
+            parent_count[tm_num][-1, -1] = t_expr_tms
         else:
-            parent_count_df.loc[
-                t_n_pa, tm_col] = (t_n_pa < parent_count_df.iloc[-1][tm_expr_col]) * 1
+            parent_count[tm_num] = np.append(
+                parent_count[tm_num], [[t_act_tms, t_expr_tms]], axis=0)
 
-        if parent_count_df.loc[t_n_pa, tm_col]:
-            parent_count_df.loc[t_n_pa, tm_expr_col] = t_expr_tms
-
-    parent_count_df.loc[t_n_pa, 't_expr'] = parent_count_df.loc[
-        t_n_pa, parent_count_df.columns[len(tms):-1]].min()
-    return parent_count_df
-
-
-def get_parents_count_dfs(parents_time_serie, parents_timescales):
-    """
-    parents_time_serie: tableau de np.array contenant la serie temporelle de chaque parent.
-    parents_timescales: tableau contenant les timescales de chaque parents, attention parents_timescales[i] doit contenir les
-    timescale du parent parents_time_serie[i]
-
-    retourne un tableau de DataFrame, un DF par parent, indiquant la configuration du parent sur une plage temporelle.
-    """
-    parents_count_dfs = []
-    for i, parent_time_serie in enumerate(parents_time_serie):
-        activation_windows_df = pd.DataFrame(data=[], index=parent_time_serie,
-                                             columns=[']{},{}]'.format(tm[1], tm[0]) for tm in parents_timescales[i]] + ['t_expr'])
-        activation_windows_df.loc[:, 't_expr'] = np.inf
-
-        # calcul la configuration du parent dans les instants où ce dernier a eu lieu
-        # ET le temps d'expiration de cette configuration.
-        activation_windows_df = count_timescale(
-            activation_windows_df, parents_timescales[i])
-        activation_windows_df = activation_windows_df.reset_index().rename(columns={
-            'index': 't'})
-        activation_windows_df.loc[:, 'parent_count'] = activation_windows_df.apply(
-            lambda r: tuple([int(single_count) for single_count in r.iloc[2:]]), axis=1)
-
-        # fusionne les intervalles de temps consécutifs sur lesquels la
-        # configuration du parent ne change pas.
-        values_g = (activation_windows_df['parent_count'] != activation_windows_df[
-                    'parent_count'].shift()).cumsum()
-        parent_count_df = activation_windows_df.groupby(['parent_count', values_g],
-                                                        as_index=False).agg({'t': 'min', 't_expr': 'max'}).sort_values('t')
-        # le temps d'expiration ne doit pas dépasser
-        # le temps de début de la prochaine configuration
-        parent_count_df['t_expr'].clip_upper(
-            parent_count_df['t'].shift(-1).fillna(parent_count_df['t'].max()), inplace=True)
-
-        parents_count_dfs.append(parent_count_df)
-
-    return parents_count_dfs
-
-
-def getParentCountVector(parent_c_df, t, nb_p_tms):
-    """
-    Trouve la configuration du parent à l'instant t à partir du DF parent_c_df
-    """
-    expr_t = np.inf
-    pcv = (0,) * nb_p_tms
-    if not parent_c_df.empty:
-        res = parent_c_df[parent_c_df.apply(
-            lambda r: t >= r['t'] and t < r['t_expr'], axis=1)]
-
-        if res.shape[0] == 0:
-            df = parent_c_df[parent_c_df['t'] > t]
-            if df.shape[0] > 0:
-                expr_t = df.iloc[-1]['t']
-        else:
-            pcv = res['parent_count'].values[0]
-            expr_t = res['t_expr'].values[0]
-    return pcv, expr_t
-
-
-def getParentsCountVector(parents_c_dfs, timescales_parents, t):
-    pc_expr = np.array([getParentCountVector(parent_count_df, t, len(timescales_parent))
-                        for parent_count_df, timescales_parent in zip(parents_c_dfs, timescales_parents)])
-    if parents_c_dfs == []:
-        return (), np.inf
-    return sum(pc_expr[:, 0], ()), min(pc_expr[:, 1])
-
-
-def get_node_pcv_from_data(model, all_time_series_df, time_serie, event=None, time_col='time', event_col='event'):
-    if event is None:
-        event = time_serie.name
-
-    parents_timeseries = [all_time_series_df.set_index(event_col).loc[parent][time_col].values
-                          for parent in model.get_node_parents(event)]
-    parents_timescales = model.get_node_parents_timescales(event)
-
-    pc_df = get_parents_count_dfs(parents_timeseries, parents_timescales)
-
-    time_serie.loc[:, 'pcv'] = time_serie[time_col].apply(lambda t: getParentsCountVector(pc_df,
-                                                                                          parents_timescales,
-                                                                                          t)[0])
-
-    return time_serie.set_index(time_col)['pcv']
-
-
-def get_nodes_pcv_from_data(model, data, event_col='event', time_col='time'):
-    return data.groupby(event_col).apply(lambda event_time_serie: get_node_pcv_from_data(model,
-                                                                                         data,
-                                                                                         event_time_serie)
-                                         ).reset_index().sort_values(by=time_col, ascending=True)['pcv']
-
-
-def lambdas_from_pcv_serie(model, all_time_series_df, event_col='event'):
-    lambda_t = all_time_series_df.apply(lambda event: model.get_lambda(event[event_col],
-                                                                       event['pcv']), axis=1)
-
-    return lambda_t
+    return parent_count

rtgemlib/rtgem.py
@@ -31,6 +31,9 @@ class RTGEM:
     def get_node_parents_timeseries(self, node):
         return [self.dpd_graph.node[pa]['timeserie'] for pa in self.dpd_graph.predecessors(node)]
 
+    def get_parents_count(self, node):
+        return [self.dpd_graph.get_edge_data(pa, node)['parent_count'] for pa in self.dpd_graph.predecessors(node)]
+
     def get_node_parents(self, node):
         return list(self.dpd_graph.predecessors(node))
 
@@ -40,14 +43,21 @@ class RTGEM:
     def get_lambdas(self, node):
         return self.dpd_graph.node[node]['lambdas']
 
+    def set_node_timeserie(self, node, ts):
+        self.dpd_graph.node[node]['timeserie'] = ts
+
     def set_lambda(self, node, pcv, lm):
         self.dpd_graph.node[node]['lambdas'][pcv] = lm
 
-    def initLambdas(self, node):
+    def initLambdas(self, node, random=True, a=0, b=10):
         nb_parent = self.get_node_nb_parents_timescales(node)
+        if random:
+            self.dpd_graph.node[node]['lambdas'] = dict(zip(list(itertools.product(
+                *[[0, 1]] * nb_parent)), np.random.uniform(a, b, np.power(2, nb_parent))))
 
-        self.dpd_graph.node[node]['lambdas'] = dict(zip(list(itertools.product(
-            *[[0, 1]] * nb_parent)), [self.default_lambda] * np.power(2, nb_parent)))
+        else:
+            self.dpd_graph.node[node]['lambdas'] = dict(zip(list(itertools.product(
+                *[[0, 1]] * nb_parent)), [self.default_lambda] * np.power(2, nb_parent)))
 
     def copy(self):
         cp = RTGEM({})
@@ -99,6 +109,7 @@ class RTGEM:
         is_possible = self.add_edge_operator_was_applied(edge)
         if is_possible:
             self.dpd_graph.remove_edge(*edge)
+            self.initLambdas(edge[1])
 
         return is_possible
 
@@ -160,7 +171,7 @@ class RTGEM:
                 valid_operations.remove(1)
             max_depth -= 1
 
-        for i in tqdm(range(max_depth)):
+        for i in tqdm(range(max_depth), leave=False):
             op_num = random.choice(valid_operations)
             if op_num == 1:
                 e = edges_to_add.pop(0)

rtgemlib/sampling.py
@@ -6,7 +6,7 @@ import numpy as np
 import math
 
 from .rtgem import RTGEM
-from .parentCount import get_parents_count_dfs, getParentsCountVector, init_parent_count_df, count_timescale, updates_parent_count_df
+from .parentCount import init_parents_count, set_parent_count, updates_parents_count, get_parents_count_vector
 
 
 def all_parents_were_sampled(tgem, scc):
@@ -18,34 +18,24 @@ def all_parents_were_sampled(tgem, scc):
     return num_sampled_pa == len(preds) - (len(scc) - 1)
 
 
-def sample_sccs_nodes(tgems, scc_nodes, t_min, t_max, pbar):
-    t = t_min
-    # nds_parents_timescales = [
-    #     tgems.get_node_parents_timescales(nd) for nd in scc_nodes]
-
-    # nds_lambdas = [tgems.dpd_graph.node[nd]['lambdas'] for nd in scc_nodes]
-
-    for nd in scc_nodes:
-        tgems.dpd_graph.node[nd]['timeserie'] = np.array([])
-
+def set_nodes_parents_counts(tgems, nodes, t_max):
+    for nd in nodes:
         for nd_pa in tgems.dpd_graph.predecessors(nd):
             pa_tms = tgems.dpd_graph.edges[(nd_pa, nd)]['timescales']
+            if 'timeserie' not in tgems.dpd_graph.nodes[nd_pa]:
+                tgems.dpd_graph.nodes[nd_pa]['timeserie'] = np.array([])
             pa_ts = tgems.dpd_graph.nodes[nd_pa]['timeserie']
-            tgems.dpd_graph.edges[(nd_pa, nd)]['parent_count'] = count_timescale(
-                init_parent_count_df(pa_ts, pa_tms), pa_tms)
+            tgems.dpd_graph.edges[(nd_pa, nd)][
+                'parent_count'] = set_parent_count(pa_ts, pa_tms, t_max)
 
-    # nds_parents_timeseries = [
-    #     tgems.get_node_parents_timeseries(nd) for nd in scc_nodes]
+    return tgems
 
-    # nds_parent_count_dfs = [get_parents_count_dfs(nd_parents_timeseries, nd_parents_timescales)
-    # for nd_parents_timeseries, nd_parents_timescales in
-    # zip(nds_parents_timeseries, nds_parents_timescales)]
 
-    # for nd_parents_count_dfs, nd_parents_timescales in
-    # zip(nds_parent_count_dfs, nds_parents_timescales)])
-
+def sample_sccs_nodes(tgems, scc_nodes, t_min, t_max, pbar):
+    t = t_min
+    tgems = set_nodes_parents_counts(tgems, scc_nodes, t_max)
     nds_pcvs_t_expirations = np.array(
-        [get_parent_configuration(tgems, nd, t) for nd in scc_nodes])
+        [get_parents_count_vector(tgems.get_parents_count(scc_nodes[i]), t, t_max) for i in range(len(scc_nodes))])
 
     lambda_t_nds = [tgems.get_lambda(scc_nodes[i], nd_pcv)
                     for i, nd_pcv in enumerate(nds_pcvs_t_expirations[:, 0])]
@@ -54,8 +44,7 @@ def sample_sccs_nodes(tgems, scc_nodes, t_min, t_max, pbar):
     t_old = t
 
     while t < t_max:
-
-        Y = np.random.uniform(size=len(scc_nodes))
+        Y = np.random.uniform(0, 1, size=len(scc_nodes))
         taus = -np.log(1 - Y) / lambda_t_nds
 
         min_tau_i = taus.argmin()
@@ -68,23 +57,23 @@ def sample_sccs_nodes(tgems, scc_nodes, t_min, t_max, pbar):
             tgems.dpd_graph.node[nd]['timeserie'] = np.append(
                 tgems.dpd_graph.node[nd]['timeserie'], t)
 
-            for nd_child in tgems.dpd_graph.successors(scc_nodes[min_tau_i]):
+            for nd_child in tgems.dpd_graph.successors(nd):
                 if nd_child in scc_nodes:
                     nd_child_id = scc_nodes.index(nd_child)
-                    if nd_child_id != -1:
-                        tgems.dpd_graph.edges[(nd, nd_child)]['parent_count'] = updates_parent_count_df(
-                            tgems.dpd_graph.edges[(nd, nd_child)]['parent_count'], tgems.dpd_graph.edges[(nd, nd_child)]['timescales'], t)
+                    tgems.dpd_graph.edges[(nd, nd_child)]['parent_count'] = updates_parents_count(
+                        tgems.dpd_graph.edges[(nd, nd_child)]['parent_count'], tgems.dpd_graph.edges[(nd, nd_child)]['timescales'], t, t_max)
 
                     nds_pcvs_t_expirations[
-                        nd_child_id, :] = get_parent_configuration(tgems, nd_child, t)
+                        nd_child_id, :] = get_parents_count_vector(
+                        tgems.get_parents_count(nd_child), t, t_max)
 
                     lambda_t_nds[nd_child_id] = tgems.get_lambda(
                         nd_child, nds_pcvs_t_expirations[nd_child_id, 0])
         else:
             t = t_change
 
-            nds_pcvs_t_expirations[min_tau_i, :] = get_parent_configuration(
-                tgems, scc_nodes[min_tau_i], t)
+            nds_pcvs_t_expirations[min_tau_i, :] = get_parents_count_vector(
+                tgems.get_parents_count(scc_nodes[min_tau_i]), t, t_max)
 
             lambda_t_nds[min_tau_i] = tgems.get_lambda(
                 scc_nodes[min_tau_i], nds_pcvs_t_expirations[min_tau_i, 0])
@@ -104,88 +93,6 @@ def sample_sccs_nodes(tgems, scc_nodes, t_min, t_max, pbar):
         tgems.dpd_graph.node[nd]['sampled'] = True
 
 
-def get_parent_configuration(tgems, node, t):
-    pcv = ()
-    t_expr = np.inf
-
-    for nd_pa in tgems.dpd_graph.predecessors(node):
-        nb_pa_tms = len(tgems.dpd_graph.edges[(nd_pa, node)]['timescales'])
-
-        parent_count_df = tgems.dpd_graph.edges[(nd_pa, node)]['parent_count']
-        gt_t = (t >= parent_count_df.index.values)
-        res = parent_count_df[
-            gt_t & (t < parent_count_df.values[:, -1])]
-
-        if res.shape[0] == 0:
-            pcv += (0,) * nb_pa_tms
-            exp_ = parent_count_df[~gt_t]
-            if exp_.shape[0] > 0:
-                t_expr = min(t_expr, exp_.iloc[0, -1])
-        else:
-            pcv += tuple(tm_cnt for tm_cnt in res.iloc[0].values[:nb_pa_tms])
-            t_expr = min(t_expr, res.iloc[0].values[-1])
-
-    return pcv, t_expr
-
-
-def sample_child_node(tgems, node, t_min, t_max, pbar):
-    # timeseries_parents = tgems.get_node_parents_timeseries(node)
-    # timescales_parents = tgems.get_node_parents_timescales(node)
-
-    # node_parents_count_dfs = get_parents_count_dfs(
-    #     timeseries_parents, timescales_parents)
-
-    t = t_min
-
-    # parent_nodes_count, t_lambda_change = getParentsCountVector(
-    #     node_parents_count_dfs, timescales_parents, t)
-    # t_lambda_change = min(t_lambda_change, t_max)
-
-    tgems.dpd_graph.node[node]['timeserie'] = np.array([])
-    for nd_pa in tgems.dpd_graph.predecessors(node):
-        pa_tms = tgems.dpd_graph.edges[(nd_pa, node)]['timescales']
-        pa_ts = tgems.dpd_graph.nodes[nd_pa]['timeserie']
-        tgems.dpd_graph.edges[(nd_pa, node)]['parent_count'] = count_timescale(
-            init_parent_count_df(pa_ts, pa_tms), pa_tms)
-
-    t_old = t_min
-    progress = 0
-    tt = 0
-    parent_nodes_count, t_lambda_change = get_parent_configuration(
-        tgems, node, t)
-
-    while t < t_max:
-        lambda_t = tgems.dpd_graph.node[node]['lambdas'][parent_nodes_count]
-
-        y = np.random.uniform()
-        waiting_time = -np.log(1 - y) / lambda_t
-
-        if t + waiting_time < t_lambda_change:
-            t += waiting_time
-            tgems.dpd_graph.node[node]['timeserie'] = np.append(
-                tgems.dpd_graph.node[node]['timeserie'], t)
-
-        else:
-            t = t_lambda_change
-            parent_nodes_count, t_lambda_change = get_parent_configuration(
-                tgems, node, t)
-
-            t_lambda_change = min(t_lambda_change, t_max)
-
-        # print('updating delta : {}'.format(math.ceil((t - t_old) * 1000)))
-        progress += ((t - t_old) / t_max) * 100
-        if (progress >= 1):
-            pbar.update(math.floor(progress))
-            tt += math.floor(progress)
-            progress = 0
-
-        t_old = t
-
-    pbar.update(pbar.total - tt)
-
-    tgems.dpd_graph.node[node]['sampled'] = True
-
-
 def sample_from_tgem(tgem, t_min=0, t_max=30):
     for nd in tgem.dpd_graph.nodes():
         tgem.dpd_graph.node[nd]['sampled'] = False
@@ -194,7 +101,7 @@ def sample_from_tgem(tgem, t_min=0, t_max=30):
     sccs = list(nx.strongly_connected_components(tgem.dpd_graph))
     i = -1
 
-    pbar = tqdm(total=len(sccs), desc='Sampled Nodes: ')
+    pbar = tqdm(total=len(sccs), desc='Sampled Nodes: ', leave=False)
     while len(sccs) > 0:
         i = (i + 1) % len(sccs)
         if all_parents_were_sampled(tgem, list(sccs[i])):
@@ -209,9 +116,9 @@ def sample_from_tgem(tgem, t_min=0, t_max=30):
     pbar.close()
 
     sampled_data_df = pd.DataFrame()
-    for nd in tqdm(tgem.dpd_graph.nodes(), desc='exporting data to DF'):
+    for nd in tqdm(tgem.dpd_graph.nodes(), desc='exporting data to DF', leave=False):
         node_sampled_tm = pd.DataFrame(
             data={'time':  tgem.dpd_graph.nodes[nd]['timeserie'], 'event': nd})
         sampled_data_df = pd.concat([sampled_data_df, node_sampled_tm])
 
-    return sampled_data_df.sort_values('time', ascending=True)
+    return sampled_data_df.reset_index(drop=True).sort_values('time', ascending=True)